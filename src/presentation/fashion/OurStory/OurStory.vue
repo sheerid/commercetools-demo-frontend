69d3fabc9d64bfa76bbc9a8c0046656c4a4a7966
Sam Ban <sam.ban@sheerid.com>
2023-02-21 11:09:55 +0000

    add minimal styling to our story

--- src/presentation/fashion/OurStory/OurStory.vue
+++ src/presentation/fashion/OurStory/OurStory.vue
@@ -2,6 +2,8 @@
 <script src="./OurStory.js"></script>
 
 <template>
-    <h1>{{ t('title') }}</h1>
-    <p>{{ t('description') }}</p>
+    <div class="mt-50 mb-80 mr-100 ml-100">
+        <h1>{{ t('title') }}</h1>
+        <p>{{ t('description') }}</p>
+    </div>
 </template>
